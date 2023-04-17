d70ac576aa99f05876f442931eeaeacaa858e031
Jitender Kumar <jkofficial1712@gmail.com>
2023-04-17 15:08:09 +0530

    uploading default ESmodule example

---
+++ ESModules/default/index.js
@@ -0,0 +1,8 @@
+import lavasa from './modl1.js'
+
+const testfn = () => {
+    lavasa();
+    console.log('I am in testfn')
+}
+
+testfn()
---
+++ ESModules/default/modl1.js
@@ -0,0 +1,5 @@
+const fn1 = () => {
+    console.log('I am in fn1')
+}
+
+export default fn1
